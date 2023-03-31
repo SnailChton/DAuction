e1570167961d7adcf3a5992c8bfe75d164c9e20c
Nikita <hahehm@mail.ru>
2023-03-31 14:28:12 +0300

    Created changing avatar function

--- digauc/routes.py
+++ digauc/routes.py
@@ -1,16 +1,7 @@
 import os
-#
-#
-#
-#
-#
+
+import PIL
 import secrets
-#
-#
-#
-#
-#
-# secrets
 from PIL import Image
 from flask import Flask, render_template, url_for, flash, redirect, request
 from digauc import app, db, bcrypt
@@ -74,12 +65,26 @@ def logout():
     return redirect(url_for('index'))
 
 
-def save_picture(form_picture):
+def save_picture(form_picture, form_username):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-    form_picture.save(picture_path)
+    if current_user.image_file != 'default.jpg':
+        os.remove(app.root_path + '\static\profile_pics\\' + current_user.image_file)
+
+    fixed_height = 480
+    i_fix = Image.open(form_picture)
+    height_percent = (fixed_height / float(i_fix.size[1]))
+    width_size = int((float(i_fix.size[0]) * float(height_percent)))
+    i_fix = i_fix.resize((width_size, fixed_height), PIL.Image.NEAREST)
+    i_fix.save(picture_path)
+
+    # methode 2(for optimizing server)
+    # output_size = (480, 480)
+    # i = Image.open(form_picture)
+    # i.thumbnail(output_size)
+    # i.save(picture_path)
 
     return picture_fn
 
@@ -90,7 +95,7 @@ def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
         if form.picture.data:
-            picture_file = save_picture(form.picture.data)
+            picture_file = save_picture(form.picture.data, form.username.data)
             current_user.image_file = picture_file
             print('picture mock_print')
         current_user.username = form.username.data
